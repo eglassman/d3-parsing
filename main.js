7c4192d063c53017fd645a67cad25d782be98ad8
Elena Glassman <elena.glassman@gmail.com>
2017-02-19 13:20:54 -0800

    extracting relevant code w/ substring

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 var esprima = require('esprima');
 var traverse = require("ast-traverse");
 
-var parsedd3 = esprima.parse('var arc = d3.arc().outerRadius(radius - 10).innerRadius(radius - 70).context(context); var labelArc = d3.arc().outerRadius(radius - 40).innerRadius(radius - 40).context(context);', { range: true, loc: true });
+var d3codeAsString = 'var arc = d3.arc().outerRadius(radius - 10).innerRadius(radius - 70).context(context); var labelArc = d3.arc().outerRadius(radius - 40).innerRadius(radius - 40).context(context);';
+
+var parsedd3 = esprima.parse(d3codeAsString, { range: true, loc: true });
 
 traverse(parsedd3, {pre: function(node, parent, prop, idx) {
-    console.log(node.type + (parent ? " from parent " + parent.type +
-        " via " + prop + (idx !== undefined ? "[" + idx + "]" : "") : ""));
+    //console.log(node.type + (parent ? " from parent " + parent.type + " via " + prop + (idx !== undefined ? "[" + idx + "]" : "") : ""));
     if (node.type == "CallExpression"){
     	console.log('check me out!');
     	console.log(node.range);
     	console.log(node.callee.property.name);
+    	console.log(d3codeAsString.slice(node.range[0],node.range[1]))
     }    
 }});

4a2348826648f8c4b5a346eb373460e29dad8c73
Elena Glassman <elena.glassman@gmail.com>
2017-02-20 09:53:09 -0800

    now getting html parsed, with locations, and getting the text back

--- htmlparser_text.js
+++ htmlparser_text.js
@@ -32,18 +32,23 @@
 
 const parse5 = require('parse5');
 const fs = require('fs');
+const options = { locationInfo: true };
 
-//var d3index = '';
+var d3index = fs.readFileSync('../d3-example-viz/data/htmlfiles/index-239.html','utf8')
+console.log(d3index)
 
-fs.readFile('../d3-example-viz/data/htmlfiles/index-239.html', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-  var document = parse5.parse(data);
-  console.log(document);
-});
+var parsedd3index = parse5.parse(d3index,options);
 
+console.log(d3index.slice(parsedd3index.childNodes[0]['__location'].startOffset,parsedd3index.childNodes[0]['__location'].endOffset))
+
+// fs.readFile('../d3-example-viz/data/htmlfiles/index-239.html', 'utf8', function (err,data) {
+//   if (err) {
+//     return console.log(err);
+//   }
+//   console.log(data);
+//   var document = parse5.parse(data);
+//   console.log(document);
+// });
 
 
 //const document = parse5.parse('<!DOCTYPE html><html><head></head><body>Hi there!</body></html>');

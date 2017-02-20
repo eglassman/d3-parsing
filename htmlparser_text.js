9864524bb02a0699a3a2cf39708470d4a1c1fe46
Elena Glassman <elena.glassman@gmail.com>
2017-02-20 02:20:32 -0800

    tried third npm package for parsing

--- htmlparser_text.js
+++ htmlparser_text.js
@@ -11,21 +11,27 @@
 // parser.parseComplete(rawHtml);
 // console.log(sys.inspect(handler.dom, false, null));
 
-var htmlparser = require("htmlparser2");
-var parser = new htmlparser.Parser({
-    onopentag: function(name, attribs){
-        if(name === "script" && attribs.type === "text/javascript"){
-            console.log("JS! Hooray!");
-        }
-    },
-    ontext: function(text){
-        console.log("-->", text);
-    },
-    onclosetag: function(tagname){
-        if(tagname === "script"){
-            console.log("That's it?!");
-        }
-    }
-}, {decodeEntities: true});
-parser.write("Xyz <script type='text/javascript'>var foo = '<<bar>>';</ script>");
-parser.end();
+// var htmlparser = require("htmlparser2");
+// var parser = new htmlparser.Parser({
+//     onopentag: function(name, attribs){
+//         if(name === "script" && attribs.type === "text/javascript"){
+//             console.log("JS! Hooray!");
+//         }
+//     },
+//     ontext: function(text){
+//         console.log("-->", text);
+//     },
+//     onclosetag: function(tagname){
+//         if(tagname === "script"){
+//             console.log("That's it?!");
+//         }
+//     }
+// }, {decodeEntities: true});
+// parser.write("Xyz <script type='text/javascript'>var foo = '<<bar>>';</ script>");
+// parser.end();
+
+const parse5 = require('parse5');
+
+const document = parse5.parse('<!DOCTYPE html><html><head></head><body>Hi there!</body></html>');
+
+console.log(document)//.childNodes[1].tagName); //> 'html'

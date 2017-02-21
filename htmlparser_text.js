ec6b19615a75c3654b7fefa4c4c8f14163473cfa
Elena Glassman <elena.glassman@gmail.com>
2017-02-21 09:02:44 -0800

    moving things around

--- htmlparser_text.js
+++ htmlparser_text.js
@@ -41,6 +41,53 @@ console.log(d3index)
 var parsedd3index = parse5.parse(d3index,options);
 console.log(utils.flatten(parsedd3index))
 
+
+for (var i in parsedd3index.childNodes) {
+    console.log('-------');
+    console.log('i',i);
+    console.log('node',parsedd3index.childNodes[i]);
+    console.log('nodename',parsedd3index.childNodes[i].nodeName);
+    console.log('loc',parsedd3index.childNodes[i]['__location']); //.startOffset,parsedd3index_flattened[i].nodeName['__location'].endOffset));
+    var iloc = parsedd3index.childNodes[i]['__location'];
+    if (iloc) {
+        console.log('extracted',d3index.slice(iloc.startOffset,iloc.endOffset));
+    } else {
+        for (var ii in parsedd3index.childNodes[i].childNodes) {
+            console.log('ii',ii);
+            console.log('node',parsedd3index.childNodes[i].childNodes[ii]);
+            console.log('nodename',parsedd3index.childNodes[i].childNodes[ii].nodeName);
+            console.log('loc',parsedd3index.childNodes[i].childNodes[ii]['__location']);
+            var iiloc = parsedd3index.childNodes[i].childNodes[ii]['__location'];
+            if (iiloc) {
+                console.log('extracted',d3index.slice(iiloc.startOffset,iiloc.endOffset));
+            } else {
+                for (var iii in parsedd3index.childNodes[i].childNodes[ii].childNodes) {
+                    console.log('iii',iii);
+                    console.log('node',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii]);
+                    console.log('nodename',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].nodeName);
+                    console.log('loc',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii]['__location']);
+                    var iiiloc = parsedd3index.childNodes[i].childNodes[ii].childNodes[iii]['__location'];
+                    if (iiiloc) {
+                        console.log('extracted',d3index.slice(iiiloc.startOffset,iiiloc.endOffset));
+                    } else {
+                        for (var iiii in parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].childNodes) {
+                            console.log('iiii',iiii);
+                            console.log('node',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].childNodes[iiii]);
+                            console.log('nodename',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].childNodes[iiii].nodeName);
+                            console.log('loc',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].childNodes[iiii]['__location']);
+                            var iiiiloc = parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].childNodes[iiii]['__location'];
+                            if (iiiiloc) {
+                                console.log('extracted',d3index.slice(iiiiloc.startOffset,iiiiloc.endOffset));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    console.log('-------');
+}
+
 //console.log(d3index.slice(parsedd3index.childNodes[0]['__location'].startOffset,parsedd3index.childNodes[0]['__location'].endOffset))
 
 // fs.readFile('../d3-example-viz/data/htmlfiles/index-239.html', 'utf8', function (err,data) {
--- main.js
+++ main.js
@@ -15,52 +15,6 @@ console.log(d3index)
 //parse the html
 var parsedd3index = parse5.parse(d3index,options);
 
-for (var i in parsedd3index.childNodes) {
-    console.log('-------');
-    console.log('i',i);
-    console.log('node',parsedd3index.childNodes[i]);
-    console.log('nodename',parsedd3index.childNodes[i].nodeName);
-    console.log('loc',parsedd3index.childNodes[i]['__location']); //.startOffset,parsedd3index_flattened[i].nodeName['__location'].endOffset));
-    var iloc = parsedd3index.childNodes[i]['__location'];
-    if (iloc) {
-        console.log('extracted',d3index.slice(iloc.startOffset,iloc.endOffset));
-    } else {
-        for (var ii in parsedd3index.childNodes[i].childNodes) {
-            console.log('ii',ii);
-            console.log('node',parsedd3index.childNodes[i].childNodes[ii]);
-            console.log('nodename',parsedd3index.childNodes[i].childNodes[ii].nodeName);
-            console.log('loc',parsedd3index.childNodes[i].childNodes[ii]['__location']);
-            var iiloc = parsedd3index.childNodes[i].childNodes[ii]['__location'];
-            if (iiloc) {
-                console.log('extracted',d3index.slice(iiloc.startOffset,iiloc.endOffset));
-            } else {
-                for (var iii in parsedd3index.childNodes[i].childNodes[ii].childNodes) {
-                    console.log('iii',iii);
-                    console.log('node',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii]);
-                    console.log('nodename',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].nodeName);
-                    console.log('loc',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii]['__location']);
-                    var iiiloc = parsedd3index.childNodes[i].childNodes[ii].childNodes[iii]['__location'];
-                    if (iiiloc) {
-                        console.log('extracted',d3index.slice(iiiloc.startOffset,iiiloc.endOffset));
-                    } else {
-                        for (var iiii in parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].childNodes) {
-                            console.log('iiii',iiii);
-                            console.log('node',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].childNodes[iiii]);
-                            console.log('nodename',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].childNodes[iiii].nodeName);
-                            console.log('loc',parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].childNodes[iiii]['__location']);
-                            var iiiiloc = parsedd3index.childNodes[i].childNodes[ii].childNodes[iii].childNodes[iiii]['__location'];
-                            if (iiiiloc) {
-                                console.log('extracted',d3index.slice(iiiiloc.startOffset,iiiiloc.endOffset));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    console.log('-------');
-}
-
 var parsedd3index_flattened = utils.flatten(parsedd3index);
 
 for (var i in parsedd3index_flattened) {
